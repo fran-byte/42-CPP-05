e6d191ea58e34b7c8250b6e73b0159a61fbf0689
fran-byte <fran-byte@hotmail.com>
2025-12-09 11:42:48 +0100

    up

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 10:57:45 by frromero          #+#    #+#             */
-/*   Updated: 2025/12/09 11:14:20 by frromero         ###   ########.fr       */
+/*   Updated: 2025/12/09 11:42:39 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,32 @@ Intern::Intern() {}
 Intern::Intern(Intern const &copy)
 {
     /*Copy anything*/
-    void(copy);
+    (void)copy;
 }
 
 Intern &Intern::operator=(Intern const &other)
 {
-    void(other);
+    (void)other;
     return *this;
 }
 
 Intern::~Intern() {}
 
-Aform *Intern::makeForm(std::string const &name, std::string const &target)
+AForm *Intern::makeForm(std::string const &name, std::string const &target)
 {
+
+    std::vector<std::string> validForm = {
+        "shrubbery creation",
+        "robotomy request",
+        "presidential pardon"}
+
+    if (std::find(validForm.begin(), validForm.end(), name) != validForm.end())
+    {
+        ; // CREAR FORMULARIO
+    }
+    else
+    {
+        ; // Emitir ERROR
+    }
+    return // Devolver Formulario
 }
--- ex03/Intern.hpp
+++ ex03/Intern.hpp
@@ -6,13 +6,13 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 10:20:42 by frromero          #+#    #+#             */
-/*   Updated: 2025/12/09 11:13:23 by frromero         ###   ########.fr       */
+/*   Updated: 2025/12/09 11:35:51 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INTERN_HPP
 #define INTERN_HPP
-#include "Aform.hpp"
+#include "AForm.hpp"
 #include <iostream>
 
 class Intern
@@ -23,7 +23,7 @@ public:
     Intern(Intern const &copy);
     ~Intern();
     Intern &operator=(Intern const &other);
-    Aform *makeForm(std::string const &name, std::string const &target);
+    AForm *makeForm(std::string const &name, std::string const &target);
 };
 
 #endif
